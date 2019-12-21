c93d583d80e69fbe93667762b9234e93a2b98522
Marcin <marcinszydelko@yahoo.com>
2019-12-21 17:41:17 +0000

    draw rectangle

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-var ctx;
+let draw = function() {
+  var ctx;
 
-ctx = document.getElementById('canvas').getContext('2d');
+  ctx = document.getElementById('canvas').getContext('2d');
+
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = 'blue';
+
+  ctx.strokeRect(100, 50, 200, 300);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(100, 50, 200, 300);
+}
